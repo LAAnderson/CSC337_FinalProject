fe3197375d31a9178c12cb35948cde6282afd7d6
LAAnderson <logananderson@arizona.edu>
2025-11-25 14:51:35 -0700

    framework for the project (very moldable)

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const crypto = require('crypto')
+const fs = require('fs')
+const url = require('url')
+const qs = require('querystring')
+
+app.listen(8080, () => {
+    console.log('Server Running on port 8080....')
+})
+
+app.get('/login', (req, res) => {
+
+})
+
+app.get('/storefront', (req, res) => {
+
+})
+
+app.get('/shoppingcart', (req, res) => {
+
+})
+
+app.get('/search', (req, res) => {
+
+})
